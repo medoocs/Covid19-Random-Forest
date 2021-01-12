9a8bc5591f1b9bf3afcb35e4574ddbad90f11b83
medoo <67025526+medoocs@users.noreply.github.com>
2021-01-12 17:29:59 +0100

    Update Covid19-RF.R
    v3

--- Covid19-RF.R
+++ Covid19-RF.R
@@ -11,23 +11,46 @@ library(sets)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 set.seed(1234)
-
-getData <- function(country, countryCode){
+dataglo <- NULL
+deleteglo <- NULL
+getData <- function(countryCode){
   dateStart = "2020-02-15"
   dateEnd = "2020-11-06"
   
   df <- download_jhu_csse_covid19_data(cached = TRUE, silent = TRUE)
   dp <- download_google_cmr_data(cached = TRUE, silent = TRUE)
-  
-  data <- na.omit(merge(df, dp, by = c("iso3c", "date")))
-  
-  mobility <- data[ which(data$date >= dateStart & data$date < dateEnd), ]
-  
-  mobility$confirmed = c(0, diff(mobility$confirmed, lag = 1))
-  for (i in nrow(mobility)) {
-    if (i > 0 && mobility[i,]$iso3c != mobility[i-1,]$iso3c) {
-      mobility[i,]$confirmed = 0;
+  if(countryCode == 'ALL'){
+    data <- na.omit(merge(df, dp, by = c("iso3c", "date")))
+    mobility <- data[ which(data$date >= dateStart & data$date < dateEnd), ]
+    mobility$confirmed = c(0, diff(mobility$confirmed, lag = 1))
+    toDelete = c()
+    for (i in 1:nrow(mobility)) {
+      if (i > 1 && mobility[i,]$iso3c != mobility[i-1,]$iso3c) {
+        mobility[i,]$confirmed = 0;
+      }else if(i > 1 && (mobility[i,]$date - 1) != mobility[i-1,]$date){
+        toDelete <- append(toDelete, i)
+      }
     }
+    if(!is.null(toDelete)){
+      mobility = mobility[-toDelete,]
+    }
+  }else{
+    df_country <- df[ which(df$iso3c == countryCode), ]
+    dp_country <- dp[ which(dp$iso3c == countryCode), ]
+    data <- na.omit(merge(df_country, dp_country, by = c("iso3c", "date")))
+    mobility <- data[ which(data$date >= dateStart & data$date < dateEnd), ]
+    mobility$confirmed = c(0, diff(mobility$confirmed, lag = 1))
+    toDelete = c()
+    for (i in 1:nrow(mobility)) {
+      if(i > 1 && (as.Date(mobility[i,]$date) - 1) != as.Date(mobility[i-1,]$date)){
+        toDelete <- append(toDelete, i)
+      }
+    }
+    deleteglo <<- toDelete
+    dataglo<<-mobility
+    if(!is.null(toDelete)){
+      mobility = mobility[-toDelete,]
+    } 
   }
   
   retail_recreation = mobility$retail_recreation
@@ -46,10 +69,9 @@ getData <- function(country, countryCode){
                      residential, 
                      confirmed)
   
-  write.csv(data, paste('alldata_', country, '.csv', sep=""))
+  write.csv(data, paste('alldata_', countryCode, '.csv', sep=""), row.names = FALSE)
   return(data)
 }
-
 globTree <- NULL
 getParams <- function(trainSet){
   
@@ -70,8 +92,8 @@ getParams <- function(trainSet){
   
   trControl <- trainControl(method = "cv",
                             number = 2,
-                            search = "grid",
-                            verboseIter = TRUE)
+                            search = "grid")
+  #                         ,verboseIter = TRUE)
   
   tuneGrid <- expand.grid(.mtry=c(1:6),
                           .maxnodes=c(seq(30,70,5)),
@@ -89,9 +111,8 @@ getParams <- function(trainSet){
   globTree <<- tree
   return(tree)
 }
-
 getModel <- function(tuneGrid, trControl, trainSet, best_maxnodes, best_ntree){
-  trained_rf <- train(confirmed~.-X,
+  trained_rf <- train(confirmed~.,
                       trainSet,
                       method = "rf",
                       metric = "RMSE",
@@ -104,44 +125,40 @@ getModel <- function(tuneGrid, trControl, trainSet, best_maxnodes, best_ntree){
   saveRDS(trained_rf, paste('rf_', country, '.rds', sep=""))
   return(trained_rf)
 }
-
 getAnaliza <- function(testSet, prediction){
   # plot real vs pred
-  plot(testSet$confirmed, col="blue")
+  plot(testSet$confirmed, col="blue", xlab = "Date", ylab = "Daily new cases", main = "Actuall vs Predicted")
   lines(prediction, col="red")
   # analiza reziduala
-  reziduali <- glm(confirmed ~ . -X, data = testSet)
+  reziduali <- glm(confirmed ~ ., data = testSet)
   print(summary(reziduali))
   # P-O dijagram
-  ggplot() + geom_point(aes(prediction, testSet$confirmed))
+  ggplot() + geom_point(aes(prediction, testSet$confirmed)) + labs(x = "Prediction", y = "Daily new cases", title = "P-O dijagram")
 }
 
 # input params
-country <- "all"
-countryCode <- "HRV"
+countryCode <- "ALL"
 
 # load data
-filename <- paste('alldata_', country, '.csv', sep="")
+filename <- paste('alldata_', countryCode, '.csv', sep="")
 if(isTRUE(file.exists(filename))){
   data <- as.data.frame(read.csv(filename, header = TRUE, sep = ','))
 }else{
-  data <- getData(country, countryCode)
+  data <- getData(countryCode)
 }
 
 # train and test (80-20)
-train <- sample(nrow(data), 0.1*nrow(data), replace = FALSE)
+train <- sample(nrow(data), 0.8*nrow(data), replace = FALSE)
 trainSet <- data[train,]
-
-filename <- paste('alldata_', 'Croatia', '.csv', sep="")
-testSet <- as.data.frame(read.csv(filename, header = TRUE, sep = ','))
+testSet <- data[-train,]
 
 # train model
-filename <- paste('rf_', country, '.rds', sep="")
+filename <- paste('rf_', countryCode, '.rds', sep="")
 if(isTRUE(file.exists(filename))){
   trained_rf <- readRDS(filename)
 }else{
   trained_rf <- getParams(trainSet)
-  saveRDS(trained_rf, paste('rf_', country, '.rds', sep=""))
+  saveRDS(trained_rf, paste('rf_', countryCode, '.rds', sep=""))
 }
 
 # predict

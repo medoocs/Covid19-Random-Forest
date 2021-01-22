7346aeea9e1da5c0a6d76b9b418916bcc3930bf6
xtactis <xtactis@gmail.com>
2021-01-22 19:36:22 +0100

    *strokes chin* ahahaaa

--- Covid19-RF.R
+++ Covid19-RF.R
@@ -1,6 +1,6 @@
 options(warn=-1) #kekw
 
-for (p in c("remotes", "tidyverse", "parallel", "doParallel", "caret", "ggplot2", "dplyr", "smoother")) {
+for (p in c("remotes", "tidyverse", "parallel", "doParallel", "caret", "ggplot2", "dplyr", "smoother", "countrycode", "shiny")) {
   if (!(p %in% rownames(installed.packages()))) {
     install.packages(p)
   }
@@ -13,21 +13,36 @@ if (!("tidycovid19" %in% rownames(installed.packages()))) {
 }
 library(tidycovid19)
 
-dateStart <- "2020-02-15"
-#dateEnd <- "2020-11-06"
-dateEnd <- "2021-01-06" # stavili smo do prvog mjeseca da bi dobili vise podataka
+#countries <- codelist$iso3c
+#names(countries) <- codelist$country.name.en
 
-tryCatch({
-  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-}, error = function(e) {
-  knitr::opts_knit$set(root.dir = dirname(rstudioapi::getActiveDocumentContext()$path))
-})
-set.seed(1234)
+#countries <- Filter(Negate(is.na), countries)
+#countries <- countries[sort(names(countries))]
+#countries <- append(c("Global Data" = "ALL"), countries)
 
-countryCode <- "HRV"
+# selectInput("countryCode", "Izbor drzave",
+#             choices=countries,
+#             selected="HRV")
+
+countryCode <- "HRV" #reactive({input$countryCode})
+
+# dateRangeInput('dateRange',
+#                label = 'Izbor datuma: dd.mm.yyyy.',
+#                format = 'dd.mm.yyyy.',
+#                start = as.Date("2020-02-15"), end = as.Date("2020-11-06")
+# )
+
+dateStart <- as.Date("2020-02-15") #reactive({input$dateRange[[1]]})
+dateEnd <- as.Date("2021-01-06")   #reactive({input$dateRange[[2]]})
+
+#checkboxInput("retrainFlag", "Ponovno u\u010Denje")
+#retrainFlag <- reactive({input$retrainFlag})
 
 processCountry <- function(data) {
-  data$confirmed = c(0, diff(data$confirmed, lag = 1)) / data$population
+  data$confirmed = c(0, diff(data$confirmed, lag = 1))
+  if (countryCode == "ALL") {
+    data$confirmed <- data$confirmed / data$population
+  }
   
   toDelete = c()
   for (i in 2:nrow(data)) {
@@ -73,8 +88,6 @@ fetchData <- function(countryCode) {
 getData <- function(countryCode) {
   data <- fetchData(countryCode)
   
-  #population <<- data$population[1]
-  
   mobility <- data[ which(data$date >= dateStart & data$date < dateEnd), ]
   
   grouped <- group_by(mobility, iso3c)
@@ -88,63 +101,72 @@ getData <- function(countryCode) {
   return(data)
 }
 
-data <- getData(countryCode)
-summary(data)
-
-getParams <- function(trainSet) {
+getParams <- function(trainSet, filename) {
+  if (isTRUE(file.exists(filename))) {
+    return(readRDS(filename))
+  }
+  
   trControl <- trainControl(method = "cv",
                             number = 5,
                             search = "grid",
-                            verboseIter = TRUE,
+                            verboseIter = FALSE,
                             allowParallel = TRUE)
   
   tuneGrid <- expand.grid(.mtry=c(1:ncol(trainSet)))
   
   registerDoParallel(cores=detectCores()-2)
   
-  tree <- train(confirmed ~ .,
-                data = trainSet,
-                method = "parRF",
-                metric = "RMSE",
-                ntree = 100,
-                maxnodes = 30,
-                tuneGrid = tuneGrid,
-                trControl = trControl,
-                importance = TRUE,
-                nodesize = 5
-  )
+  tree <- caret::train(confirmed ~ .,
+                       data = trainSet,
+                       method = "parRF",
+                       metric = "RMSE",
+                       ntree = 100,
+                       maxnodes = 30,
+                       tuneGrid = tuneGrid,
+                       trControl = trControl,
+                       importance = TRUE,
+                       nodesize = 5)
   
   stopImplicitCluster()
+  
+  saveRDS(tree, filename)
   
   return(tree)
 }
 
+plotLines <- function(testSet, prediction) {
+  # plot real vs pred
+  plot(testSet$confirmed, type="l", lwd=3, col="#ff904c", xlab = "Index", ylab = "Daily new cases", main = paste("Actual vs Predicted (", countryCode, ")", sep=""))
+  lines(prediction, col="#39a767", lwd=3)
+  lines(smth.gaussian(prediction, window=7, tails=TRUE, alpha=3.0), col="#6e4f7b", lwd=3)
+  legend("topleft", legend=c("actual (smooth)", "predicted", "predicted (smooth)"), col=c("#ff904c", "#39a767", "#6e4f7b"), lty=1, cex=0.8, lwd=3)
+}
+
+plotPO <- function(testSet, prediction) {
+  # P-O dijagram
+  ggplot() + geom_point(aes(prediction, testSet$confirmed)) + labs(x = "Prediction", y = "Daily new cases", title = "P-O dijagram")
+}
+
+data <- getData(countryCode)
+summary(data)
+
+set.seed(1234)
 # train and test (80-20)
 train <- sample(nrow(data), 0.8*nrow(data), replace = FALSE)
+
 trainSet <- data[train,]
 testSet <- data[-train,]
 
 # train model
 filename <- paste('rf_', countryCode, '.rds', sep="")
-if(isTRUE(file.exists(filename))){
-  trained_rf <- readRDS(filename)
-}else{
-  trained_rf <- getParams(trainSet)
-  saveRDS(trained_rf, paste('rf_', countryCode, '.rds', sep=""))
-}
+
+trained_rf <- getParams(trainSet, filename)
 summary(trained_rf)
 
-getAnaliza <- function(testSet, prediction) {
-  # plot real vs pred
-  plot(testSet$confirmed, type="l", lwd=3, col="blue", xlab = "Date", ylab = "Daily new cases", main = "Actual vs Predicted")
-  #lines(testSet$confirmed, col="blue")
-  lines(prediction, col="red", lwd=3)
-  legend("topleft", legend=c("actual (smooth)", "predicted"), col=c("blue", "red"), lty=1, cex=0.8)
-  # P-O dijagram
-  ggplot() + geom_point(aes(prediction, testSet$confirmed)) + labs(x = "Prediction", y = "Daily new cases", title = "P-O dijagram")
-}
 prediction <- predict(trained_rf, testSet)
-getAnaliza(testSet, prediction)
+
+plotLines(testSet, prediction)
+plotPO(testSet, prediction)
 
 reziduali <- glm(confirmed ~ ., data = testSet)
 summary(reziduali)
